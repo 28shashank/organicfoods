e9ae28bbac5d8b6fa39bb68bc2c0f688b54f92ee
Shashank Shekhar <28.shashank@gmail.com>
2021-01-11 13:49:15 +0530

    created organic food landing page first commit

---
+++ js/custom-script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: false,
+    });
+    $(".add-to-cart").on('click', function (event) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        $("#shopping-cart").addClass("shake");
+        setTimeout(function () {
+            $("#shopping-cart").removeClass("shake");
+        }, 1000);
+    });
+
+    // dropdown list for mobile view
+    $(".dropdown-menu li a").click(function () {
+        var selText = $(this).text();
+        $(this).parents('.btn-group').find('.dropdown-toggle').html(selText + ' <span class="caret"></span>');
+    });
+
+});
+
+// Page scroll add and remove class
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $(".header_area").addClass("darkHeader");
+    } else {
+        $(".header_area").removeClass("darkHeader");
+    }
+});
